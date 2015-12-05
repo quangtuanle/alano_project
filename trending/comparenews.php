3444cb593755c85366eb5fe5346f652927dfd38b
trungnguyen281 <ndtrunga195@gmail.com>
2015-12-05 12:13:15 +0700

    Update comparenews.php

--- trending/comparenews.php
+++ trending/comparenews.php
@@ -1,25 +1,63 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
 <?php
+function compare(array $TinTuc1, array $TinTuc2)
+{
+	//Compare TieuDe
+	$tieude1 = explode(" ", $TinTuc1["TieuDe"]);
+	$tieude2 = explode(" ", $TinTuc2["TieuDe"]);
+	
+	$count1 = 0;
+	for ($i = 0; $i < count($tieude1); $i++)
+		for ($j = 0; $j < count($tieude2); $j++)
+			if ($tieude1[$i] == $tieude2[$j])
+				$count1++;
+	
+	//Compare TomTat
+	$tomtat1 = explode(" ", $TinTuc1["TomTat"]);
+	$tomtat2 = explode(" ", $TinTuc2["TomTat"]);
+	
+	$count2 = 0;
+	for ($i = 0; $i < count($tomtat1); $i++)
+		for ($j = 0; $j < count($tomtat2); $j++)
+			if ($tomtat1[$i] == $tomtat2[$j])
+				$count2++;
+			
+	//Compare Tag
+	$tag1 = explode(" ", $TinTuc1["Tag"]);
+	$tag2 = explode(" ", $TinTuc2["Tag"]);
+	
+	$count3 = 0;
+	for ($i = 0; $i < count($tag1); $i++)
+		for ($j = 0; $j < count($tag2); $j++)
+			if ($tag1[$i] == $tag2[$j])
+				$count3++;
+			
+	$avg = 0.4*$count1/count($tieude1) + 0.25*$count2/count($tomtat1) + 0.35*$count3/count($tag1);
+	
+	if ($avg >= 0.75)
+		return true;
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "alano";
+$database = "alano";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connectionection
+$connection = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check connectionection
+if ($connection->connect_error) {
+    die("connectionection failed: " . $connection->connect_error);
 }
 else
 {
-	mysqli_query($conn,"SET NAMES utf8"); 
+	mysqli_query($connection,"SET NAMES utf8"); 
 
 	//Read from MySql Database
-	$sql = "select MaTinTuc, TieuDe, TomTat, NoidungChinh, TacGia, NgayDang, DuongLinkGoc, TinhTrangLink, CacTrangDanNguon, SoLuotShare, SoLuotComment, BaiTuongTu, SoBaiTrung from TinTuc";
-	$result = mysqli_query($conn,$sql);
+	$sql = "SELECT MaTinTuc, TieuDe, TomTat, NoidungChinh, NgayDang, DuongLinkGoc, TinhTrangLink, CacTrangDanNguon, SoLuotShare, SoLuotComment, BaiTuongTu, SoBaiTrung, Tag FROM TinTuc";
+	$result = mysqli_query($connection,$sql);
 	
 	$TinTucInfo = array();
 	
@@ -31,7 +69,7 @@ else
 	for ($i = 0; $i < count($TinTucInfo) - 1; $i++)
 		for ($j = $i + 1; $j < count($TinTucInfo); $j++)
 		{
-			if ($TinTucInfo[$i]["TieuDe"] == $TinTucInfo[$j]["TieuDe"])
+			if (compare($TinTucInfo[$i],$TinTucInfo[$j]))
 			{
 				$TinTucInfo[$i]["SoBaiTrung"] += 1;
 				$TinTucInfo[$j]["SoBaiTrung"] += 1;
@@ -42,8 +80,10 @@ else
 	foreach($TinTucInfo as $tintuc)
 	{
 		$sqlud = "update TinTuc set SoBaiTrung = " . $tintuc["SoBaiTrung"] . " where MaTinTuc = " . $tintuc["MaTinTuc"];
-		$result = mysqli_query($conn,$sqlud);
+		$result = mysqli_query($connection,$sqlud);
 	}
+	
+	$connection->close();
 	
 	echo "Complete!";
 }
