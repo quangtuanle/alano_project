ebd1aae507d70c303191dfbe68e5a91a1686002c
quangtuanle <lequangtuan270695@gmail.com>
2015-12-06 10:43:02 +0700

    Create index.php

---
+++ server-site/WebALANO/index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<title>ALANO</title>
+	</head>
+	
+	<body>
+		<div class="status"></div>
+		<div class="wrapper">
+			<?php
+				include('modules/config.php');
+				include('modules/header.php');
+				include('modules/menu.php');
+				include('modules/content.php');
+				include('modules/footer.php');
+			?>
+		</div>	
+	</body>
+</html>
