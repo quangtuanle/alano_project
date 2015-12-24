05e9f9ad4ccc86490a8323a767c9818e075208de
trungnguyen281 <ndtrunga195@gmail.com>
2015-12-24 15:05:36 +0700

    update
    Đổi tên các thuộc tính tiếng Anh

--- trending/comparenews.php
+++ trending/comparenews.php
@@ -16,43 +16,54 @@ function sameRatio ($string1, $string2)
 	return $count/count($word1);
 }
 
+//Kiểm tra từ viết hoa
+function isUpper($a)
+{
+	return ($a[0] >= 'A' && $a[0] <= 'Z');
+}
+
 //Hàm so sánh 2 tin tức
-function compare(array $TinTuc1, array $TinTuc2)
+function compare(array $News1, array $News2)
 {
 	//So sánh tiêu đề 2 bài viết (Đếm số từ giống nhau)
-	$tieude = sameRatio($TinTuc1["TieuDe"],$TinTuc2["TieuDe"]);
+	$title = sameRatio($News1["title"],$News2["title"]);
 	
 	//So sánh phần tóm tắt
-	$tomtat = 0;//sameRatio($TinTuc1["TomTat"],$TinTuc2["TomTat"]);
+	$summary = 0;//sameRatio($News1["summary"],$News2["summary"]);
 	
 	//Đếm số tag giống nhau
-	$tag = sameRatio($TinTuc1["Tag"],$TinTuc2["Tag"]);
+	$tag = sameRatio($News1["tag"],$News2["tag"]);
 	
-	//So sánh nội dung chính, chỉ so sánh các danh từ riêng	
-	$noidung1 = preg_split("/[ \n\t,.:']/", $TinTuc1["NoidungChinh"], -1, PREG_SPLIT_NO_EMPTY);
-	$noidung2 = preg_split("/[ \n\t,.:']/", $TinTuc2["NoidungChinh"], -1, PREG_SPLIT_NO_EMPTY);
-		
+	//So sánh nội dung chính, chỉ so sánh các danh từ riêng
+	$content1 = preg_split("/[ \n\t,.:']/", $News1["content"], -1, PREG_SPLIT_NO_EMPTY);
+	$content2 = preg_split("/[ \n\t,.:']/", $News2["content"], -1, PREG_SPLIT_NO_EMPTY);
+
 	$countupper = 0;
 	$sameword = 0;
-	for ($i = 0; $i < count($noidung1); $i++)
+	for ($i = 1; $i < count($content1) - 1; $i++)
 	{
-		if ($noidung1[$i] >= 'A' && $noidung1[$i] <= 'Z')
+		if (isUpper($content1[$i]) && (isUpper($content1[$i-1]) || isUpper($content1[$i+1])))
 		{
 			$countupper++;
 			
-			for ($j = 0; $j < count($noidung2); $j++)
-				if ($noidung1[$i] == $noidung2[$j])
+			for ($j = 0; $j < count($content2); $j++)
+				if ($content1[$i] == $content2[$j])
 				{
 					$sameword++;
 					break;
 				}
 		}
 	}
-		
-	$noidung = $sameword/$countupper;
 	
-	$avg = 0.4*$tieude + 0.25*$noidung + 0.15*$tomtat + 0.2*$tag;
-
+	if ($countupper == 0)
+		$content = 0;
+	else
+		$content = $sameword/$countupper;
+	
+	$avg = 0.4*$title + 0.25*$content + 0.15*$summary + 0.2*$tag;
+	
+	//echo $avg . "<br/>";
+	
 	if ($avg >= 0.65)
 		return true;
 }
@@ -60,7 +71,7 @@ function compare(array $TinTuc1, array $TinTuc2)
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "alano";
+$database = "alano_website";
 
 // Tạo kết nối đến CSDL
 $connection = new mysqli($servername, $username, $password, $database);
@@ -73,74 +84,87 @@ else
 {
 	mysqli_query($connection,"SET NAMES utf8"); 
 
-	//Đọc dữ liệu từ bảng Tin Tức
-	$sql = "SELECT MaTinTuc, TieuDe, TomTat, NoidungChinh, NgayDang, DuongLinkGoc, TinhTrangLink, CacTrangDanNguon, SoLuotShare, SoLuotComment, BaiTuongTu, SoBaiTrung, Tag, BaiGoc, DaDuyet FROM TinTuc";
+	//Đọc dữ liệu từ bảng News
+	$sql = "SELECT * FROM News";
 	$result = mysqli_query($connection,$sql);
 	
-	$TinTucInfo = array();
+	$NewsInfo = array();
 	
-	while($row_TinTuc = mysqli_fetch_assoc($result))
-		$TinTucInfo[] = $row_TinTuc;
-
+	while($row_News = mysqli_fetch_assoc($result))
+		$NewsInfo[] = $row_News;
 	
 	//So sánh 2 tin tức
-	for ($i = 0; $i < count($TinTucInfo); $i++)
+	for ($i = 0; $i < count($NewsInfo); $i++)
 	{
-		if ($TinTucInfo[$i]["DaDuyet"] == 0)	//Chỉ so sánh những tin chưa duyệt
+		if ($NewsInfo[$i]["reviewed"] == 0)	//Chỉ so sánh những tin chưa duyệt
 		{
-			for ($j = $i + 1; $j < count($TinTucInfo); $j++)
+			for ($j = $i + 1; $j < count($NewsInfo); $j++)
 			{
+				//echo $i . "so với " . $j . ": ";
 				//Nếu 2 tin được cho là có nội dung giống nhau
-				if (compare($TinTucInfo[$i],$TinTucInfo[$j]))
-				{					
+				if (compare($NewsInfo[$i],$NewsInfo[$j]))
+				{	
+					echo $i . "Giống" . $j;
+					
 					//Tăng số bài trùng của mỗi tin lên 1
-					$TinTucInfo[$i]["SoBaiTrung"] += 1;
-					$TinTucInfo[$j]["SoBaiTrung"] += 1;
+					$NewsInfo[$i]["num_same"] += 1;
+					$NewsInfo[$j]["num_same"] += 1;
+					
+					//Nếu là tin đã duyệt, cập nhật lại độ hot
+					if ($NewsInfo[$j]["original"] == 1)
+						$NewsInfo[$j]["hot"] += 1;	
 				
 					//Gán bài gốc cho bài đăng sớm hơn
-					$d1 = date_parse_from_format('Y-m-d H:i:s',$TinTucInfo[$i]["NgayDang"]);
-					$d2 = date_parse_from_format('Y-m-d H:i:s',$TinTucInfo[$j]["NgayDang"]);
+					$d1 = date_parse_from_format('Y-m-d H:i:s',$NewsInfo[$i]["published_at"]);
+					$d2 = date_parse_from_format('Y-m-d H:i:s',$NewsInfo[$j]["published_at"]);
 					
-					if ($TinTucInfo[$i]["BaiTuongTu"] == null && $TinTucInfo[$j]["BaiTuongTu"] == null)
+					if ($NewsInfo[$i]["same_news"] == null && $NewsInfo[$j]["same_news"] == null)
 					{
 						if ($d1 < $d2)
-							$TinTucInfo[$i]["BaiGoc"] = 1;
+							$NewsInfo[$i]["original"] = 1;
 						else
-							$TinTucInfo[$j]["BaiGoc"] = 1;
+							$NewsInfo[$j]["original"] = 1;
 					}
 					else
 					{
-						if ($d1 < $d2 && $TinTucInfo[$i]["BaiGoc"] == 0)
+						if ($d1 < $d2 && $NewsInfo[$i]["original"] == 0)
 						{
-							$temp = $TinTucInfo[$i]["BaiGoc"];
-							$TinTucInfo[$i]["BaiGoc"] = $TinTucInfo[$j]["BaiGoc"];
-							$TinTucInfo[$j]["BaiGoc"] = $temp;
+							$temp = $NewsInfo[$i]["original"];
+							$NewsInfo[$i]["original"] = $NewsInfo[$j]["original"];
+							$NewsInfo[$j]["original"] = $temp;
 						}
 						
 					}
 					
 					//Đưa vào danh sách các bài tương tự
-					$TinTucInfo[$i]["BaiTuongTu"] = $TinTucInfo[$i]["BaiTuongTu"] . $TinTucInfo[$j]["MaTinTuc"] . " ";
-					$TinTucInfo[$j]["BaiTuongTu"] = $TinTucInfo[$j]["BaiTuongTu"] . $TinTucInfo[$i]["MaTinTuc"] . " ";
+					$NewsInfo[$i]["same_news"] = $NewsInfo[$i]["same_news"] . $NewsInfo[$j]["id"] . " ";
+					$NewsInfo[$j]["same_news"] = $NewsInfo[$j]["same_news"] . $NewsInfo[$i]["id"] . " ";
 				}
 			}
+			//Tính độ hot
+			$NewsInfo[$i]["hot"] = $NewsInfo[$i]["num_like"]*2 + $NewsInfo[$i]["num_share"]*5 + $NewsInfo[$i]["num_comment"] + $NewsInfo[$i]["num_same"]*5;
 			
 			//Đã duyệt
-			$TinTucInfo[$i]["DaDuyet"] = 1;
+			$NewsInfo[$i]["reviewed"] = 1;
 		}
 		
 		//Nếu là bài duy nhất, không có bài tương tự thì đó là bài gốc
-		if ($TinTucInfo[$i]["BaiTuongTu"] == null)
-			$TinTucInfo[$i]["BaiGoc"] = 1;
+		if ($NewsInfo[$i]["same_news"] == null)
+			$NewsInfo[$i]["original"] = 1;
 	}
 	
 	//Cập nhật lại dữ liệu
-	foreach($TinTucInfo as $tintuc)
+	foreach($NewsInfo as $News)
 	{
-		$sqlud = "update TinTuc set SoBaiTrung = " . $tintuc["SoBaiTrung"] . ", BaiTuongTu = '" . $tintuc["BaiTuongTu"] . "', BaiGoc = " . $tintuc["BaiGoc"] . ", DaDuyet = " . $tintuc["DaDuyet"] . " where MaTinTuc = " . $tintuc["MaTinTuc"];
+		$sqlud = "update News set num_same = " . $News["num_same"] . ", same_news = '" . $News["same_news"] . "', original = " . $News["original"] . ", reviewed = " . $News["reviewed"] . ", hot = " . $News["hot"] . " where id = " . $News["id"];
+		//$sqlud = "update News set num_same = 0, same_news = null, original = 0, reviewed = 0, hot = 0 where id = " . $News["id"];
+		//echo $sqlud . "<br>";
+		
 		$result = mysqli_query($connection,$sqlud);
 	}
 	
 	$connection->close();
+	
+	//echo "Complete!";
 }
 ?>
